918195ef827f797f3311952d483a51743a03c6ea
Mehdi Saffar <saffar.mehdi3@gmail.com>
2021-08-23 16:20:31 +0300

    build client ssr

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,3 +1,14 @@
-export default function Index() {
+import axios from 'axios'
+import buildClient from '../api/build-client'
+
+export default function Index({ currentUser }) {
+    console.log('component currentUser', currentUser)
     return <h1>Landing Page</h1>
+}
+
+Index.getInitialProps = async ctx => {
+    const client = buildClient(ctx)
+    const { data } = await client.get('/api/users/currentuser')
+
+    return data
 }
