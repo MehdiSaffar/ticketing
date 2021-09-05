db4f2d7601ea21984a060e8368308efa138a5e77
Mehdi Saffar <saffar.mehdi3@gmail.com>
2021-09-05 15:08:33 +0300

    add expiration srv

---
+++ expiration/src/index.ts
@@ -0,0 +1,38 @@
+import { natsWrapper } from './nats-wrapper'
+
+const envKeys = ['NATS_URL', 'NATS_CLUSTER_ID', 'NATS_CLIENT_ID']
+
+for (const envKey of envKeys) {
+    if (!process.env[envKey]) {
+        throw new Error(`Missing environment variable ${envKey}`)
+    }
+}
+
+const start = async () => {
+    try {
+        await natsWrapper.connect(
+            process.env.NATS_CLUSTER_ID!,
+            process.env.NATS_CLIENT_ID!,
+            process.env.NATS_URL!
+        )
+        console.log('Connected to NATS')
+
+        // ---
+
+        natsWrapper.client.on('close', () => {
+            console.log('NATS connection closed')
+        })
+
+        const shutdown = async () => {
+            natsWrapper.client.close()
+            process.exit(0)
+        }
+
+        process.on('SIGINT', shutdown)
+        process.on('SIGTERM', shutdown)
+    } catch (e) {
+        console.error(e)
+    }
+}
+
+start()
